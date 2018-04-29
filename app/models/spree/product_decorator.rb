bb463beffc70f3ebb515bafd5458e84d34b1e8d6
askl56 <amscott@openmailbox.org>
2018-04-29 05:28:19 -0400

    Use delegate as opposed to delegate_belongs_to

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Product.class_eval do
   Spree::PermittedAttributes.product_attributes.concat [:variant_tag_ids]
 
-  delegate_belongs_to :master, :variant_tags, :variant_tag_ids, :all_tags
+  delegate :master, :variant_tags, :variant_tag_ids, :all_tags
 end
